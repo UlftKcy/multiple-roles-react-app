e9d705336325ca016c9f4f85a155ef9eee0aec34
ulfet <ukacay@albasoft.com.tr>
2023-01-20 18:12:55 +0300

    updated register test

--- src/__tests__/Register.test.jsx
+++ src/__tests__/Register.test.jsx
@@ -1,22 +1,60 @@
-import { findByText, getByText, render, waitFor } from "@testing-library/react";
-import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 import Register from "../pages/auth/Register";
 
-describe("<Register/> tests", () => {
-    test("renders Register page", async () => {
-        const register = { name: "Register" };
-        const routes = [
-            {
-                path: '/register',
-                element: <Register />
-            },
-        ];
-        const router = createMemoryRouter(routes, {
-            initialEntries: ['/register'],
-        });
-        render(<RouterProvider router={router} />)
-        
-        const submitButton = await findByText('submit');
-        await waitFor(() => expect(submitButton)).toHaveTextContent(register.name)
+describe("Register tests", () => {
+    const handleSubmit = jest.fn();
+    const user = userEvent.setup();
+
+    beforeEach(() => {
+        handleSubmit.mockClear();
     });
-})
+
+    afterEach(() => {
+        cleanup();
+        jest.resetAllMocks();
+    });
+
+    test("renders Register page", () => {
+        const registerRoute = '/register';
+        render(
+            <MemoryRouter initialEntries={[registerRoute]}>
+                <Register />
+            </MemoryRouter>
+        )
+        expect(screen.getByTestId("btn_register_submit")).toHaveTextContent("Register");
+    });
+
+    test("onSubmit is called when all inputs pass validation", async () => {
+        const registerRoute = '/register';
+        render(
+            <MemoryRouter initialEntries={[registerRoute]}>
+                <Register onSubmit={handleSubmit} />
+            </MemoryRouter>
+        );
+        await user.type(screen.getByLabelText(/first name/i), 'Ulfet');
+        await user.type(screen.getByLabelText(/last name/i), 'Kacay');
+        await user.type(screen.getByLabelText(/occupation/i), 'Engineer');
+        await user.type(screen.getByLabelText(/phone/i), '05451110022');
+        await user.type(screen.getByLabelText(/email/i), 'test@test.com');
+        await user.type(screen.getByLabelText(/password/i), '12345678');
+
+        await user.click(screen.getByRole('button', { name: /register/i }));
+
+        await waitFor(() => {
+            expect(handleSubmit).toHaveBeenCalledWith({
+                first_name: "Ulfet",
+                last_name: "Kacay",
+                occupation: "Engineer",
+                phone: "05451110022",
+                email: "test@test.com",
+                password: "12345678",
+            });
+        });
+        await waitFor(() => {
+            expect(handleSubmit).toHaveBeenCalledTimes(1);
+        });
+
+    });
+});
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,18 @@
-import { Box, Button, Container, FormControl, FormLabel, Heading, Input, Link, Text, VStack } from '@chakra-ui/react';
-import { useFormik } from 'formik';
-import React from 'react';
-import { validate } from '../../validation/registerValidate';
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import React from "react";
+import { validate } from "../../validation/registerValidate";
 import { Link as ReachLink } from "react-router-dom";
 
 const Register = () => {
@@ -10,111 +21,166 @@ const Register = () => {
       first_name: "",
       last_name: "",
       occupation: "",
-      email: "",
       phone: "",
+      email: "",
       password: "",
     },
     validate,
     onSubmit: (values) => {
-      console.log(values)
+      console.log(values);
     },
-  })
+  });
   return (
-    <Container minH="100vh" display="flex" justifyContent="center" alignItems="center" my={{ base: '14', md: '0' }}>
+    <Container
+      minH="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      my={{ base: "14", md: "0" }}
+    >
       <Box bg="white" p={10} rounded="md" w="100%">
-        <Heading textAlign="center" mb={6}>Register</Heading>
+        <Heading textAlign="center" mb={6}>
+          Register
+        </Heading>
         <form onSubmit={formik.handleSubmit}>
           <VStack spacing={5}>
             <FormControl>
-              <FormLabel htmlFor='first_name'>First Name</FormLabel>
+              <FormLabel htmlFor="first_name">First Name</FormLabel>
               <Input
                 id="first_name"
                 name="first_name"
                 type="text"
-                {...formik.getFieldProps("first_name")} /* useFormik() returns a helper method called formik.getFieldProps() to make it faster to wire up inputs */
-                style={{ borderColor: `${formik.touched.first_name && formik.errors.first_name ? "red" : ""}` }}
+                placeholder="First Name"
+                {...formik.getFieldProps(
+                  "first_name"
+                )} /* useFormik() returns a helper method called formik.getFieldProps() to make it faster to wire up inputs */
+                style={{
+                  borderColor: `${
+                    formik.touched.first_name && formik.errors.first_name
+                      ? "red"
+                      : ""
+                  }`,
+                }}
               />
               {formik.touched.first_name && formik.errors.first_name ? (
-                <div style={{ color: "red" }}>{formik.errors.first_name}</div>
+                <div data-testid="first_name_error" style={{ color: "red" }}>{formik.errors.first_name}</div>
               ) : null}
             </FormControl>
             <FormControl>
-              <FormLabel htmlFor='last_name'>Last Name</FormLabel>
+              <FormLabel htmlFor="last_name">Last Name</FormLabel>
               <Input
                 id="last_name"
                 name="last_name"
                 type="text"
+                placeholder="Last Name"
                 {...formik.getFieldProps("last_name")}
-                style={{ borderColor: `${formik.touched.last_name && formik.errors.last_name ? "red" : ""}` }}
+                style={{
+                  borderColor: `${
+                    formik.touched.last_name && formik.errors.last_name
+                      ? "red"
+                      : ""
+                  }`,
+                }}
               />
               {formik.touched.last_name && formik.errors.last_name ? (
                 <div style={{ color: "red" }}>{formik.errors.last_name}</div>
               ) : null}
             </FormControl>
             <FormControl>
-              <FormLabel htmlFor='occupation'>Occupation</FormLabel>
+              <FormLabel htmlFor="occupation">Occupation</FormLabel>
               <Input
                 id="occupation"
                 name="occupation"
                 type="text"
+                placeholder="Occupation"
                 {...formik.getFieldProps("occupation")}
-                style={{ borderColor: `${formik.touched.occupation && formik.errors.occupation ? "red" : ""}` }}
+                style={{
+                  borderColor: `${
+                    formik.touched.occupation && formik.errors.occupation
+                      ? "red"
+                      : ""
+                  }`,
+                }}
               />
               {formik.touched.occupation && formik.errors.occupation ? (
                 <div style={{ color: "red" }}>{formik.errors.occupation}</div>
               ) : null}
             </FormControl>
             <FormControl>
-              <FormLabel htmlFor='phone'>Phone</FormLabel>
+              <FormLabel htmlFor="phone">Phone</FormLabel>
               <Input
                 id="phone"
                 name="phone"
                 type="tel"
+                placeholder="Phone"
                 {...formik.getFieldProps("phone")}
-                style={{ borderColor: `${formik.touched.phone && formik.errors.phone ? "red" : ""}` }}
+                style={{
+                  borderColor: `${
+                    formik.touched.phone && formik.errors.phone ? "red" : ""
+                  }`,
+                }}
               />
               {formik.touched.phone && formik.errors.phone ? (
                 <div style={{ color: "red" }}>{formik.errors.phone}</div>
               ) : null}
             </FormControl>
             <FormControl>
-              <FormLabel htmlFor='email'>Email</FormLabel>
+              <FormLabel htmlFor="email">Email</FormLabel>
               <Input
                 id="email"
                 name="email"
                 type="email"
+                placeholder="Email"
                 {...formik.getFieldProps("email")}
-                style={{ borderColor: `${formik.touched.email && formik.errors.email ? "red" : ""}` }}
+                style={{
+                  borderColor: `${
+                    formik.touched.email && formik.errors.email ? "red" : ""
+                  }`,
+                }}
               />
               {formik.touched.email && formik.errors.email ? (
                 <div style={{ color: "red" }}>{formik.errors.email}</div>
               ) : null}
             </FormControl>
             <FormControl>
-              <FormLabel htmlFor='password'>Password</FormLabel>
+              <FormLabel htmlFor="password">Password</FormLabel>
               <Input
                 id="password"
                 name="password"
                 type="password"
+                placeholder="Password"
                 {...formik.getFieldProps("password")}
-                style={{ borderColor: `${formik.touched.password && formik.errors.password ? "red" : ""}` }}
+                style={{
+                  borderColor: `${
+                    formik.touched.password && formik.errors.password
+                      ? "red"
+                      : ""
+                  }`,
+                }}
               />
               {formik.touched.password && formik.errors.password ? (
                 <div style={{ color: "red" }}>{formik.errors.password}</div>
               ) : null}
             </FormControl>
-            <Button type="submit" colorScheme="purple" width="full">
+            <Button
+              data-testid="btn_register_submit"
+              type="submit"
+              colorScheme="purple"
+              width="full"
+            >
               Register
             </Button>
           </VStack>
         </form>
         <Box display="flex" justifyContent="center" alignItems="center" my={4}>
           <Text mr={3}>Do you have an account?</Text>
-          <Link fontSize="lg" fontWeight="extrabold" as={ReachLink} to="/login">Login</Link>
+          <Link fontSize="lg" fontWeight="extrabold" as={ReachLink} to="/login">
+            Login
+          </Link>
         </Box>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
 export default Register;
